f0b51f16344d6c72986669e78de302e1bd927ed8
framaschool <96589704+framaschool@users.noreply.github.com>
2022-01-29 11:55:36 +0100

    Update exchange.sh

--- upgrade/2.1.0/exchange.sh
+++ upgrade/2.1.0/exchange.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-cd ~/ark-core
-pm2 delete ark-core
+cd ~/kapu-core
+pm2 delete kapu-core
 pm2 delete ark-core-relay
 git reset --hard
 git pull
@@ -9,4 +9,4 @@ git checkout master
 yarn run bootstrap
 yarn run upgrade
 
-pm2 --name 'ark-core-relay' start ~/ark-core/packages/core/dist/index.js -- relay --network mainnet
+pm2 --name 'ark-core-relay' start ~/kapu-core/packages/core/dist/index.js -- relay --network mainnet
